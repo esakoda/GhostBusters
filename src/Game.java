45d33d0c544de1ad60a25b72eba2ffc4d10f03fe
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-04-20 13:38:36 -0700

    Added the state instance variables and the constants for each state in Game.

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,15 @@ public class Game {
     private int spawnInterval;
     private GameView window;
 
+    private int state;
+
+    public static final int STATE_TITLE = 0;
+    public static final int STATE_INSTRUCTIONS = 1;
+    public static final int STATE_GAME = 2;
+    public static final int STATE_END = 3;
+
     public Game(){
+        state = STATE_GAME;
         window = new GameView(this);
     }
 
@@ -36,5 +44,9 @@ public class Game {
 
     public static void main(String[] args){
         Game ghostBusters = new Game();
+    }
+
+    public int getState() {
+        return state;
     }
 }
--- src/GameView.java
+++ src/GameView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameView extends JFrame {
     private Game backend;
@@ -12,5 +13,26 @@ public class GameView extends JFrame {
         this.setTitle("GhostBusters");
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        int state = backend.getState();
+
+        if (state == Game.STATE_TITLE) {
+            drawTitle(g);
+        } else if (state == Game.STATE_INSTRUCTIONS) {
+            drawInstructions(g);
+        } else if (state == Game.STATE_GAME) {
+            // TODO
+        } else if (state == Game.STATE_END) {
+
+        }
+    }
+
+    public void drawTitle(Graphics g) {
+
+    }
+    public void drawInstructions(Graphics g) {
+
     }
 }
