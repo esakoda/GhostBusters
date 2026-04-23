656f11aabfc9da2077267d793a30bd98130f7c55
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-04-22 18:17:35 -0700

    Saved the colored ghost images into an array. Added to the game state to draw rows and col of ghosts.

--- src/GameView.java
+++ src/GameView.java
@@ -6,8 +6,19 @@ public class GameView extends JFrame {
     private final int WINDOW_WIDTH = 1000;
     private final int WINDOW_HEIGHT = 800;
 
+    private Image[] ghostImages = new Image[6];
+
     public GameView(Game backend){
         this.backend = backend;
+
+        // Initialize an array of different colored ghost images
+        ghostImages[0] = new ImageIcon("resources/redGhost.png").getImage();
+        ghostImages[1] = new ImageIcon("resources/orangeGhost.png").getImage();
+        ghostImages[2] = new ImageIcon("resources/yellowGhost.png").getImage();
+        ghostImages[3] = new ImageIcon("resources/greenGhost.png").getImage();
+        ghostImages[4] = new ImageIcon("resources/blueGhost.png").getImage();
+        ghostImages[5] = new ImageIcon("resources/purpleGhost.png").getImage();
+
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("GhostBusters");
@@ -23,7 +34,18 @@ public class GameView extends JFrame {
         } else if (state == Game.STATE_INSTRUCTIONS) {
             drawInstructions(g);
         } else if (state == Game.STATE_GAME) {
-            // TODO
+            Ghost[][] ghosts = backend.getGhosts();
+
+            // Iterate through 2D Ghost array and print it out
+            for (Ghost[] ghost_row: ghosts) {
+                for (Ghost currGhost : ghost_row) {
+                    Image ghostImage = ghostImages[currGhost.getColorIndex()];
+
+                    // Draw ghost with it's x and y coor
+                    g.drawImage(ghostImage, currGhost.getX(), currGhost.getY(), this);
+                }
+            }
+
         } else if (state == Game.STATE_END) {
 
         }
