192b885e4b562ab6418bdb666c878919575b4cde
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-04-22 18:16:34 -0700

    Initialized 2D array of ghosts, setting up random color, starting x, and y coor. Added getter methods.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 public class Game {
     private Ghost[][] ghosts;
+
     private LaserPack laserPack;
     private int score;
     private boolean gameOver;
@@ -14,23 +15,34 @@ public class Game {
     public static final int STATE_END = 3;
 
     // Create a constant array of ghost colors
-    public static final String[] ghost_colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
+    public static final String[] ghostColors = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
 
     public Game(){
         this.state = STATE_GAME;
+        // TODO: TEMPORARY SOLUTION
+        // Create a 2D array, representing a grid of ghosts
+        this.ghosts = new Ghost[7][4];
+
+        // Index through the empty array to create individual ghost objects
+        for (int row = 0; row < ghosts.length; row++) {
+            for (int col = 0; col < ghosts[row].length; col++) {
+                int randomColorIndex = (int) (Math.random() * 6);
+
+                // Calculate each ghost's starting x and y positions based on row/col
+                int startX = col * 110 + 550;
+                int startY = row * 100 + 50;
+                ghosts[row][col] = new Ghost(randomColorIndex, startX, startY);
+            }
+        }
+
         this.window = new GameView(this);
 
         // TODO: add constants for the row/col size
-        // Create a 2D array, representing a grid of ghosts
-        this.ghosts = new Ghost[3][7];
+    }
 
-        // Index through the empty array to create individual ghost objects
-        for (Ghost[] row: ghosts) {
-            for (Ghost g: row) {
-                int random_color_index = (int) (Math.random() * 6);
-                g = new Ghost(window, ghost_colors[random_color_index]);
-            }
-        }
+    // Getter methods
+    public Ghost[][] getGhosts() {
+        return ghosts;
     }
 
     public void deleteGhost(){
