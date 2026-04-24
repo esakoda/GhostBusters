f9f12c5b4fa377b8854417093c7e669cef0d4277
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-04-24 09:09:33 -0700

    Fixed Ellen's pull request comment.s Added constants for the UI.

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,16 @@ public class Game {
 
     private int state;
 
+    // Magic Numbers for 2D Array
+    public static final int GHOST_ROWS = 7;
+    public static final int GHOST_COL = 4;
+
+    public static final int GHOST_X_SPACING = 110;
+    public static final int GHOST_Y_SPACING = 100;
+    public static final int GHOST_X_INITIAL = 550;
+    public static final int GHOST_Y_INITIAL = 50;
+
+
     public static final int STATE_TITLE = 0;
     public static final int STATE_INSTRUCTIONS = 1;
     public static final int STATE_GAME = 2;
@@ -21,7 +31,7 @@ public class Game {
         this.state = STATE_GAME;
         // TODO: TEMPORARY SOLUTION
         // Create a 2D array, representing a grid of ghosts
-        this.ghosts = new Ghost[7][4];
+        this.ghosts = new Ghost[GHOST_ROWS][GHOST_COL];
 
         // Index through the empty array to create individual ghost objects
         for (int row = 0; row < ghosts.length; row++) {
@@ -29,8 +39,8 @@ public class Game {
                 int randomColorIndex = (int) (Math.random() * 6);
 
                 // Calculate each ghost's starting x and y positions based on row/col
-                int startX = col * 110 + 550;
-                int startY = row * 100 + 50;
+                int startX = col * GHOST_X_SPACING + GHOST_X_INITIAL;
+                int startY = row * GHOST_Y_SPACING + GHOST_Y_INITIAL;
                 ghosts[row][col] = new Ghost(randomColorIndex, startX, startY);
             }
         }
